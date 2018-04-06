d602d563917d83c6808b26c33124d6181a976944
Artur Côrte-Real <artur.corte-real@innowave.tech>
2018-04-06 16:58:06 +0100

    convert to component based architecture

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';                                // add
-import { Observable } from 'rxjs/Observable';                               // add
-
-import { AddTodo, ToggleTodo } from '@state/todo-list/todo-list.actions';   // add
-import { State } from '@state/todo-list/todo-list.state';                   // add
 
 @Component({
     selector: 'app-todo-app',
@@ -12,25 +7,9 @@ import { State } from '@state/todo-list/todo-list.state';                   // a
 })
 export class TodoAppComponent implements OnInit {
 
-    newTodo: string;
-    todoList$: Observable<State>;
-
-    constructor(private store: Store<State>) {                                  // change
-        this.todoList$ = store.pipe(select('todoList'));                        // add
-    }
+    constructor() {}
 
     ngOnInit() {
     }
 
-    addTodo() {
-        this.store.dispatch(                                                    // change
-            new AddTodo({ name: this.newTodo })                                 // change
-        );                                                                      // change
-    }
-
-    toggleTodo(index: number) {                                                 // add
-        this.store.dispatch(                                                    // add
-            new ToggleTodo({ index })                                           // add
-        );                                                                      // add
-    }
 }
