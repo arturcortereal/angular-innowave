f363f75e6195bd19d58ceab9cf3900911204897f
Artur Côrte-Real <artur.corte-real@innowave.tech>
2018-04-06 17:09:34 +0100

    added routes

--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TodoAppComponent } from './todo-app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';        // add
 
 const routes: Routes = [{
     path: '',
     component: TodoAppComponent,
+    children: [                                                             // add
+        { path: ':index', component: TodoItemComponent },                   // add
+    ]                                                                       // add
 }];
 
 @NgModule({
