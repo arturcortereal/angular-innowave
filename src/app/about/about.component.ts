9de7c05f36f1651af6418bf132350f04fffae650
Artur Côrte-Real <artur.corte-real@innowave.tech>
2018-04-06 15:20:54 +0100

    setup redux

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';                            // add
+import { Observable } from 'rxjs/Observable';
+
+import { Increment, Decrement } from '@state/counter/counter.actions';  // add
+import { State } from '@state/counter/counter.state';                   // add
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    count$: Observable<number>;                                         // add
 
-  ngOnInit() {
-  }
+    constructor(private store: Store<State>) {                          // change
+        this.count$ = store.pipe(select('count'));                      // add
+    }
+
+    increment() {                                                        // add
+        this.store.dispatch(new Increment());                           // add
+    }                                                                   // add
+
+    decrement() {                                                        // add
+        this.store.dispatch(new Decrement());                           // add
+    }                                                                   // add
+
+    ngOnInit() {
+    }
 
 }
